68e487c2651b34d41daadf965546ddc8a4640628
martinalejandroo25 <122748008+martinalejandroo25@users.noreply.github.com>
2026-09-08 11:11:17 +0200

    Script de suspension

--- suspend-desktop.sh
+++ suspend-desktop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Script para suspender el escritorio de forma remota y segura
-
+# Script para suspender el escritorio de forma remota
+tailscale = 'martin@100.109.119.118'
 echo "Enviando comando de suspensión al escritorio (100.109.119.118)..."
 echo "La sesión se cerrará y el equipo se suspenderá en 5 segundos."
-ssh martin@100.109.119.118 "(sleep 5 && systemctl suspend) & disown"
+ssh $tailscale "(sleep 5 && systemctl suspend) & disown"
 echo "Comando enviado."
